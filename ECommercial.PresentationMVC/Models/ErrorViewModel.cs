b1c153e13521b4324a253ad345138559328b874e
Deniz Demirkan <denizdemirkan.code@gmail.com>
2023-08-08 17:01:15 +0300

    Initialize

---
+++ ECommercial.PresentationMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommercial.PresentationMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
